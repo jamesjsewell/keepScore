674e7c60beb183cbe6b388cd35a94dfdc4384ccb
james sewell <jamesjosephsewell@gmail.com>
2017-04-05 10:56:14 -0500

    added boilerplate actions for the arena logic

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -4,107 +4,13 @@ import User from './models/userModel.js'
 
 const ACTIONS = {
 
-	// addGame: function(issueData) {
-	// 	issueData.userName = User.getCurrentUser().get('name')
-	// 	issueData.userId = User.getCurrentUser().get('_id')
-	// 	var newIssue = new IssueModel(issueData) // creates a 
-	// 		// new instance of IssueModel, setting the issueData
-	// 		// from the form as its .attributes. this data (username, 
-	// 		// status, etc.) is what backbone will put into the body
-	// 		// of the post request when we use .save()
-
-	// 	newIssue.save() // backbone will here submit a post request
-	// 		// on our behalf.
-	// 		.then(
-	// 			// .then can actually take two callbacks, one to 
-	// 				// handle a good response, and one to handle
-	// 				// an error.
-	// 			function(response) { // SUCCESS
-	// 				alert('saved one for ya!')
-	// 				ACTIONS.fetchIssues()
-	// 			},
-	// 			function(err) { // FAILURE
-	// 				alert('problem saving your issue!')
-	// 				console.log(err)
-	// 			}
-	// 		)
-	// },
-
-	// addLike: function(model) {
-	// 	model.set({
-	// 		likes: model.get('likes') + 1
-	// 	})
-	// 	model.save() // since the model is old, i.e. it already 
-	// 	// has an id, backbone will send a PUT request here instead 
-	// 	// of a POST request
-	// 	// AND, since the model has a urlRoot and an idAttribute,
-	// 	// backbone will anticipate our needs and stick the model's _id
-	// 	// onto the end of the url. 
-	// 		.done(function(resp) {
-	// 			console.log(resp)
-	// 			ACTIONS.fetchIssues()
-	// 		})
-	// 		.fail(function(err) {
-	// 			alert('couldn\'t register your like')
-	// 			console.log(err)
-	// 		})
-	// },
-
-	// deleteMod: function(model) {
-	// 	model.destroy()
-	// 		.done(ACTIONS.fetchIssues)
-	// 		.fail(
-	// 			function(err) {
-	// 				alert('problem deleting your model!')
-	// 				console.log(err)
-	// 			})
-	// },
-
-	// fetchIssues: function() {
-	// 	// we allow this function to be used with or without 
-	// 		// a query object that will filter our fetch. 
-	// 		// if no queryObject is passed in, it will be undefined,
-	// 		// and we'll set it to be empty object.
-
-	// 	var issueColl = STORE.get('issueCollection')
-	// 	// backbone && jquery, on our behalf, will add a "GET" 
-	// 	// verb to the header of our request when we use 
-	// 	// .fetch()
-	// 	issueColl.fetch()
-	// 		.then(function() {
-	// 			STORE.set({
-	// 				issueCollection: issueColl
-	// 			})
-	// 		})
-	// },
-
-	// fetchIssuesByUser: function(inputId) {
-	// 	var issueColl = STORE.get('issueCollection')
-	// 	// backbone && jquery, on our behalf, will add a "GET" 
-	// 	// verb to the header of our request when we use 
-	// 	// .fetch()
-
-	// 	// with a data object set for fetch's input options object, we can
-	// 		// add key-value pairs to the query string
-	// 	// e.g. /api/issues?userId=132lk5lhjlk4
-	// 	issueColl.fetch({
-	// 		data: {
-	// 			userId: inputId
-	// 			} 
-	// 	})
-	// 		.then(function() {
-	// 			STORE.set({
-	// 				issueCollection: issueColl
-	// 			})
-	// 		})
-	// },
 	getUserId: function(){
 		console.log(User.getCurrentUser().attributes._id)
 		return User.getCurrentUser().attributes._id
 
 	},
 
-	fetchArenas: function(){
+	fetch_arenas: function(){
 
 		// 	// we allow this function to be used with or without 
 		// 		// a query object that will filter our fetch. 
@@ -124,7 +30,7 @@ const ACTIONS = {
 
 	},
 
-	getArenasForUser: function(id){
+	get_arenas_for_user: function(id){
 
 		var arenaModels = STORE.get('arenaCollection')
 
@@ -155,6 +61,51 @@ const ACTIONS = {
 				return(arenasArray)
 				
 			})	
+
+	},
+
+	//ACTIONS FOR THE ARENA
+	fetch_matches_for_arena: function(arenaId){
+
+
+	},
+
+	//user can only belong to this match
+	return_current_match_for_user: function(arenaId, playerId){
+
+
+	},
+
+	//there is only one active match at any given time
+	return_active_match_for_arena: function(arenaId){
+
+	},
+
+	//sets completed matches for the arena
+	return_completed_matches_for_arena: function(arenaId){
+
+	},
+
+	//USER ACTIONS
+
+	//Arena Logic
+	create_match: function(){
+
+	},
+
+	add_match_to_queue: function(){
+
+	},
+
+	move_match_up_down: function(){
+
+	},
+
+	remove_match_from_queue: function(){
+
+	},
+
+	complete_match: function(){
 
 	},
 
